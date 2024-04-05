62541c7fffcf344a9eaad1a5726f15c7e2c65f15
szwos <szwos5@gmail.com>
2024-04-05 20:34:10 +0200

    implemented Tree data structure

---
+++ AnimationLibrary/tree/DrawableTree.py
@@ -0,0 +1,9 @@
+#TODO: maybe make a fork of pyTree library and put this code as ported to python 3
+
+#TODO: check if this Tree even works XD
+
+#TODO:
+# create DrawableTree class, that will use Tree class, but will have smanim specific operations available (encapsulation basically)
+# try to make DrawableObject class, that will implement Node class, so that it can be stored on the tree, but this object will also only have library specific operations
+# this object will be a general object, that all objects that can be drawn (rasterized / appear on screen) will inherit after
+
