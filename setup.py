ed30f644e572fcca2066d41392b432e09f950638
szwos <szwos5@gmail.com>
2024-07-25 19:48:52 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='smanim',
+    version='1',
+    packages=['AnimationLibrary'],
+    url='https://github.com/szwos/smanim',
+    license='',
+    author='Szwos',
+    author_email='szwos5@gmail.com',
+    description='smanim - small library of objects for creating simple mathematic animations. Written in Python 3 for my engineer\'s thesis. '
+)
